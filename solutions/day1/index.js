2c88022c1107ea8b276fa85b53bddb8abd8d901b
Sheng Yunzhou <shengyunzhou@gmail.com>
2018-03-04 12:54:21 +0800

    Add solution to Part 2 of Day 1

--- solutions/day1/index.js
+++ solutions/day1/index.js
@@ -9,3 +9,13 @@ inputArray.forEach((value, index) => {
 });
 
 console.log(`Part one result is ${partOneResult}`);
+
+let partTwoResult = 0;
+
+inputArray.forEach((value, index) => {
+    const nextIndexCandidate = index + inputArray.length / 2;
+    const nextIndex = nextIndexCandidate >= inputArray.length ? nextIndexCandidate - inputArray.length : nextIndexCandidate;
+    partTwoResult += value === inputArray[nextIndex] ? value : 0;
+});
+
+console.log(`Part two result is ${partTwoResult}`);
